3275cc14f4850838178d0ca2f3acbb5e44f8ead6
Youssef Gude <74066684+Stardenburdenhardenbart@users.noreply.github.com>
2021-11-25 21:27:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+app.get('/rechner/:operator', (req,res) => {
+    const { x, y } = req.body
+    const { operator } = req.params
+    var answer = null
+    if (operator == "addition") {
+        answer = x+y
+    }
+    else if (operator == "subtraktion"){
+        answer = x-y
+    }
+    else if (operator == "multiplikation"){
+        answer = x*y
+    }
+    else if (operator == "division"){
+        answer = x/y
+    }
+    else if (operator == "root"){
+        answer = Math.pow(x, 1/y)
+    }
+    res.status(200).send({
+        answer: answer,
+        operator: operator
+    });
+});
+app.get('/rechner/:derivate', (req,res) => {
+    const { x, y } = req.body
+    const { operator } = req.params
+    var answer = null
+    if (operator == "addition") {
+        answer = x+y
+    }
+    else if (operator == "subtraktion"){
+        answer = x-y
+    }
+    else if (operator == "multiplikation"){
+        answer = x*y
+    }
+    else if (operator == "division"){
+        answer = x/y
+    }
+    else if (operator == "root"){
+        answer = Math.pow(x, 1/y)
+    }
+    res.status(200).send({
+        answer: answer,
+        operator: operator
+    });
+});
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.use( express.json() )
+
+app.listen(
+    PORT
+)
+
+app.get('/tshirt', (req,res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'large',
+    });
+});
+
+
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({message: 'We need a logo!'})
+    }
+
+    res.send({
+       tshirt: `👕 with your ${logo} and ID of ${id}`,
+    });
+
+});
